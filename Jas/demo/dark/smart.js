860ded33d54f91704dc933b626283e34e59ca8d9
Jason <jason_ishac@hotmail.com>
2022-09-21 19:13:32 +0300

    First Smart Data Sucsseful Modification

--- Jas/demo/dark/smart.js
+++ Jas/demo/dark/smart.js
@@ -5,13 +5,14 @@ var Original_Scontent = `
 <div class="col-md-3">
    <div class="blog">
    
-   <input id="input" placeholder="123-45-678"></input>
+   <input type="text" id="input" placeholder="Enter Data">
+   <a button id="AddData" class="btn btn" onclick="addstudent()">Add Data</a></button>
   
-   <a id="AddData" class="btn btn">Add Data</a>
-      <select>
-         <option style="color:red" value="">Enter Data</option>
+ 
+      <select style="color:black" value=""Show Data>
+         <option value="" disabled selected hidden>Show Data</option>
          <option >test1</option>
-         <option >test2</option>
+         
       </select>
       
    </div>
@@ -23,7 +24,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
    .then(json => {
       json.forEach(element => {
          var Scontent = Original_Scontent;
-         Scontent = Scontent.replace()
+         Scontent = Scontent.replace('test1',element.name)
 
          var smarttt = document.createElement('S_Div')
          smarttt.innerHTML = Scontent
